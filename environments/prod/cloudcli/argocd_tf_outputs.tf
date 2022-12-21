c800cdb7f7d1bfcb70c0ac068e31e50ab96ad6e3
Ori Hoch <ori@uumpa.com>
2022-12-21 08:51:54 +0200

    remove the default ingress cloudcli domain

--- environments/prod/cloudcli/argocd_tf_outputs.tf
+++
@@ -1,9 +0,0 @@
-resource "kubernetes_config_map" "tf_outputs" {
-  metadata {
-    name = "tf-outputs-cloudcli"
-    namespace = "argocd"
-  }
-  data = {
-    cloudcli_server_domain = "${var.sub_domain}.${var.root_domain}"
-  }
-}
--- environments/prod/cloudcli/main.tf
+++ environments/prod/cloudcli/main.tf
@@ -1,20 +1,3 @@
 terraform {
   backend "pg" {}
 }
-
-terraform {
-  required_providers {
-    cloudflare = {
-      source  = "cloudflare/cloudflare"
-    }
-  }
-}
-
-provider "kubernetes" {
-  config_path = "~/.kube/config"
-  config_context = "cloudcli-prod"
-}
-
-variable "root_domain" {}
-variable "sub_domain" {}
-variable "ingress_hostname" {}

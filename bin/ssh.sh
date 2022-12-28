89d8fcffc3eafb934ee5e0f241917987fe79b8e0
Ori Hoch <ori@uumpa.com>
2022-12-28 18:53:49 +0200

    add sshd config initcontainer to cluster-admin daemonset

--- bin/ssh.sh
+++ bin/ssh.sh
@@ -5,7 +5,7 @@ SCRIPT="${2}"
 
 SSH_ACCESS_POINT_IP="$(bin/terraform.py output -raw ssh_access_point_public_ip --root-environment)"
 
-if [ "${SERVER}" == "ssh_access_point" ]; then
+if [ "${SERVER}" == "ssh_access_point" ] || ([ "${SERVER}" == "" ] && [ "${SCRIPT}" == "" ]); then
   exec ssh -i "${TF_VAR_ssh_private_key_file}" root@$SSH_ACCESS_POINT_IP "${SCRIPT}"
 else
   exec ssh -ti "${TF_VAR_ssh_private_key_file}" root@$SSH_ACCESS_POINT_IP "ssh $SERVER '${SCRIPT}'"
